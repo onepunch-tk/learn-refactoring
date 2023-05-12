02cbb6039bafaf1842ee8ebc5cd9990246dbd5e5
TaekyungHa <86tkstar@gmail.com>
2023-05-12 17:00:52 +0900

    Chapter 1 - Function extraction test completed

--- __test__/chapter1.test.js
+++ __test__/chapter1.test.js
@@ -1,9 +1,16 @@
 import expect from "expect";
 import statement from "../chapters/1";
-//datas
 import { invoices, plays } from "../datas/chapter1";
+//datas
 
-test("test", () => {
-  expect(1).toBe(1);
+test("bill printing", () => {
+  expect(statement(invoices, plays)).toBe(
+    "청구 내역 (고객명: BigCo)\n" +
+      "Hamlet: $650.00 (55석)\n" +
+      "As You Like It: $580.00 (35석)\n" +
+      "Othello: $500.00 (40석)\n" +
+      "총액: $1,730.00\n" +
+      "적립 포인트: 47점\n"
+  );
   console.log(statement(invoices, plays));
 });
--- chapters/1.js
+++ chapters/1.js
@@ -1,6 +1,7 @@
 /*
  * 프로그램이 새로운 기능을 추가하기에 편한 구조가 아니라면,
- * 먼저 기능을 추가하기 쉬운 형태로 리팩터링하고 나서 원하는 기능을 추가한다.*/
+ * 먼저 기능을 추가하기 쉬운 형태로 리팩터링하고 나서 원하는 기능을 추가한다.
+ * 리팩터링을 실행하기 전에 제대로 된 테스트부터 마련한다. jest...*/
 
 /*origin*/
 function statement(invoice, plays) {
@@ -14,25 +15,29 @@ function statement(invoice, plays) {
   }).format;
   invoice.performance.forEach((perf) => {
     const play = plays[perf.playID];
-    let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy": //비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": //희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    //origin
+    //let thisAmount = 0;
+
+    // switch (play.type) {
+    //   case "tragedy": //비극
+    //     thisAmount = 40000;
+    //     if (perf.audience > 30) {
+    //       thisAmount += 1000 * (perf.audience - 30);
+    //     }
+    //     break;
+    //   case "comedy": //희극
+    //     thisAmount = 30000;
+    //     if (perf.audience > 20) {
+    //       thisAmount += 10000 + 500 * (perf.audience - 20);
+    //     }
+    //     thisAmount += 300 * perf.audience;
+    //     break;
+    //   default:
+    //     throw new Error(`알 수 없는 장르: ${play.type}`);
+    // }
+    //Extract Function 호출
+    let thisAmount = amountFor(perf, play);
 
     //포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -51,6 +56,39 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+}
+
+/*첫 번째
+ * statement() 처럼 긴 함수를 리팩터링할 때는
+ * 먼저 전체 동작을 각각의 부분으로 나눌 수 있는 지점을 찾는다.
+ * switch 문 리팩터링 (Extract Function)
+ * 함수를 추출할 때 유효범위를 벗어나는 변수 즉, 추출한 함수에서 곧바로 사용할 수 없는 변수가 있는지 확인
+ * statement() 에서는 perf, play, thisAmount 가 유효 범위에서 벗어남
+ * perf와 play는 값을 변경 시키지 않으므로 매개변수로 전달
+ * thisAmount의 경우 값을 변경하므로 마지막에 변경된 thisAmount를 반환하도록 작성*/
+
+function amountFor(perf, play) {
+  let thisAmount = 0; //변수를 초기화하는 코드
+
+  switch (play.type) {
+    case "tragedy": //비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": //희극
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return thisAmount; //함수 안에서 값이 바뀌는 변수 반환.
 }
 
 export default statement;
