380c5b20cb81e3656b8d5b85ac61410693325878
TaekyungHa <86tkstar@gmail.com>
2023-05-12 15:38:54 +0900

    first commit

---
+++ datas/chapter1.js
@@ -0,0 +1,14 @@
+export const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  asLike: { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
+
+export const invoices = {
+  customer: "BigCo",
+  performance: [
+    { playID: "hamlet", audience: 55 },
+    { playID: "asLike", audience: 35 },
+    { playID: "othello", audience: 40 },
+  ],
+};
---
+++ index.js
@@ -0,0 +1,6 @@
+import statement from "./chapters/1.js";
+
+//datas
+import { invoices, plays } from "./datas/chapter1.js";
+
+console.log(statement(invoices, plays));
