9c4c6d0e5b03a2d30d780dc392ffb38a840590f4
TaekyungHa <86tkstar@gmail.com>
2023-05-12 16:28:45 +0900

    chapter1 test

--- __test__/chapter1.test.js
+++ __test__/chapter1.test.js
@@ -1,6 +1,9 @@
-//datas
 import expect from "expect";
+import statement from "../chapters/1";
+//datas
+import { invoices, plays } from "../datas/chapter1";
 
 test("test", () => {
   expect(1).toBe(1);
+  console.log(statement(invoices, plays));
 });
